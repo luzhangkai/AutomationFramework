f77f56a31e1d9e90825a0d24e3d68f083a38c511
luzhangkai <luzhangkai1993@163.com>
2018-05-04 16:06:35 +0800

    Actions

--- src/main/java/com/lzk/actions/Actions.java
+++ src/main/java/com/lzk/actions/Actions.java
@@ -3,6 +3,10 @@ package com.lzk.actions;
 import com.lzk.drivers.SeleniumDriver;
 import com.lzk.find.WebElementUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Actions extends SeleniumDriver {
     //点击方法
@@ -13,5 +17,20 @@ public class Actions extends SeleniumDriver {
     public static  void sendKeys(By by,String text){
         WebElementUtils.findElement(by).clear();
         WebElementUtils.findElement(by).sendKeys(text);
+    }
+    //获取文本
+    public  static  String getText(By by){
+        String text = WebElementUtils.findElement(by).getText();
+        return  text;
+    }
+    //获取文本集
+    public  static ArrayList getTexts(By by){
+        ArrayList arrayList = new ArrayList();
+        List<WebElement> list = WebElementUtils.findElements(by);
+        for(int i=0;i<list.size();i++){
+            String text = list.get(i).getText();
+            arrayList.add(text);
+        }
+        return  arrayList;
     }
 }
--- src/test/java/com/lzk/drivers/ActionsTest.java
+++ src/test/java/com/lzk/drivers/ActionsTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 public class ActionsTest {
     @Test
     public void test(){
@@ -13,5 +15,14 @@ public class ActionsTest {
         Actions.sendKeys(By.name("wd"),"selenium");
         Actions.click(By.id("su"));
 
+    }
+    @Test
+    public  void test2(){
+        WebDriver driver = SeleniumDriver.open("chrome");
+        driver.get("file:///F:/webdriver_demo/selenium_html/index.html");
+        ArrayList arrayList = Actions.getTexts(By.xpath("//*[@id=\"moreSelect\"]/option"));
+        for(int i=0;i<arrayList.size();i++){
+            System.out.println(arrayList.get(i));
+        }
     }
 }
